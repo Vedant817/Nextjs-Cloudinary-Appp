32c16bd75bacfdcbff66c17ed2d2489a3a879ceb
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-08-18 13:04:58 +0530

    Home and Video Upload

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -5,7 +5,7 @@ import { getCldImageUrl, getCldVideoUrl } from 'next-cloudinary'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import { filesize } from 'filesize'
-import { Video } from '@prisma/client'
+import { Video } from '@/types';
 
 dayjs.extend(relativeTime)
 
